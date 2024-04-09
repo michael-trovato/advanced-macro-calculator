204fbafa550eff0e44170d5dfe6670acf61f00e7
Michael Trovato <michaeltrovato@protonmail.com>
2024-04-08 21:19:35 -0500

    Update simple macro calculator to support metric system and improved macros calculation
    This update includes support for user inputs in metric units (kilograms and centimeters) and extends the macro nutrients calculations to consider Omega-3 and linoleic acid intake. The calculator now determines lean body mass in a more comprehensive way depending on the user's sex, age, height and known body fat percentage. The weight change input has been extended to support percentages, pounds or kilograms of intended loss or gain per week.

--- simple-macro-calculator.py
+++ simple-macro-calculator.py
@@ -1,3 +1,5 @@
+import re
+
 IDEAL_BFP_MALE = 12.5
 IDEAL_BFP_FEMALE = 22.5
 MALE_INITIAL_WEIGHT = 110
@@ -6,15 +8,71 @@ INITIAL_HEIGHT = 60
 CALORIES_PER_G_FAT = 9
 CALORIES_PER_G_CARB = 4
 CALORIES_PER_G_PROTEIN = 4
-CALORIES_PER_LB_BODY_FAT = 3500
-MAX_LBS_LOSS_PER_WEEK = -2
-MAX_LBS_GAIN_PER_WEEK = 1
-GRAM_PROTEIN_LEAN_BODY_MASS = 1.2
-MINIMUM_FAT_BODY_WEIGHT = 0.3
+CALORIES_PER_KG_BODY_FAT = 7700
+MAX_KG_LOSS_PER_WEEK = -0.907185
+MAX_KG_GAIN_PER_WEEK = 0.453592
+GRAM_PROTEIN_LEAN_BODY_MASS_KG = 2.2
+MIN_FAT_G_LBM_KG = 0.75
+MIN_FAT_SAFETY_FACTOR_PERCENTAGE = 0.2
+OMEGA3_INTAKE_G_MIN = 1.75
+OMEGA3_INTAKE_G_MAX = 2.5
+LINOLEIC_ACID_AI_AGE_THRESHOLD = 50
+LINOLEIC_ACID_AI = {
+    'm': [17, 14],
+    'f': [12, 11]
+}
+LBM_POP_DIST_MIN = 45  # 100 lbs
+LBM_POP_DIST_MAX = 91  # 200 lbs
+
+def lbs_to_kg(lbs: float) -> float:
+    return lbs / 2.205
+
+def kg_to_lbs(kgs: float) -> float:
+    return kgs / 0.4536
+
+def cm_to_inches(cms: float) -> float:
+    return cms * 0.393701
+
+def inches_to_cm(inches: float) -> float:
+    return inches * 2.54
+
+def parse_input(user_input, available_units):
+    # Define the regex pattern to match numbers and units
+    units = '|'.join(available_units)
+    pattern = r"([-]?\d+\.?\d*)\s*(" + units + ")"
+
+    # Search for matches in the user input
+    match = re.search(pattern, user_input)
+
+    if match:
+        # Extract the number and unit from the matched groups
+        number = float(match.group(1))  # Convert number to float
+        unit = match.group(2)
+        return number, unit
+    else:
+        raise ValueError()
+
+
+def calculate_fatty_acids(sex, age, lean_body_mass):
+    ratio = (lean_body_mass - LBM_POP_DIST_MIN) / (LBM_POP_DIST_MAX - LBM_POP_DIST_MIN)
+    omega3 = OMEGA3_INTAKE_G_MIN + (ratio * (OMEGA3_INTAKE_G_MAX - OMEGA3_INTAKE_G_MIN))
+
+    if age < LINOLEIC_ACID_AI_AGE_THRESHOLD:
+        linoleic_acid = LINOLEIC_ACID_AI[sex][0]
+    else:
+        linoleic_acid = LINOLEIC_ACID_AI[sex][1]
+
+    total = lean_body_mass * MIN_FAT_G_LBM_KG * (1 + MIN_FAT_SAFETY_FACTOR_PERCENTAGE)
+
+    if total < omega3 + linoleic_acid:
+        total = omega3 + linoleic_acid
+
+    return total, omega3, linoleic_acid
 
 
 print("Press Ctrl-C to quit...")
 
+# TDEE
 while True:
     tdee = None
     try:
@@ -28,12 +86,14 @@ while True:
         continue
     break
 
+# Weight
 while True:
     print()
     try:
-        weight = int(input('What is your weight in lbs? '))
+        weight_input = input('What is your weight (units: lbs or kg)? ')
+        weight, weight_units = parse_input(weight_input, available_units=['lbs', 'kg'])
     except ValueError:
-        print('Error: Please enter a number.')
+        print('Error: Please enter a number with lbs or kg as units.')
         continue
     if weight <= 0:
         print('Error: Please enter a positive number.')
@@ -53,8 +113,10 @@ while True:
 
 body_fat_percentage = None
 height = None
+height_units = None
 sex = None
 if bfp_known == 'y':
+    # Get BFP
     while True:
         print()
         try:
@@ -67,31 +129,44 @@ if bfp_known == 'y':
             continue
         break
 
-# Get height & sex if BFP isn't known
+# Get height if BFP isn't known
 elif bfp_known == 'n':
+    # Height
     while True:
         print()
         try:
-            height = int(input('What is your height in inches? '))
+            height_input = input('What is your height (units: inches or cm)? ')
+            height, height_units = parse_input(height_input, available_units=['inches', 'cm'])
         except ValueError:
-            print('Error: Please enter a number.')
+            print('Error: Please enter a number with inches or cm as units.')
             continue
         if height <= 0:
             print('Error: Please enter a positive number.')
             continue
         break
 
-    while True:
-        print()
-        sex = input('What is your sex (M/F)? ').lower()
-        if sex == 'm':
-            sex = 1
-            break
-        elif sex == 'f':
-            sex = 0
-            break
-        else:
-            print('Error: Please enter either M or F.')
+# Sex
+while True:
+    print()
+    sex = input('What is your sex (M/F)? ').lower()
+    if sex == 'm':
+        break
+    elif sex == 'f':
+        break
+    else:
+        print('Error: Please enter either M or F.')
+
+# Age
+while True:
+    print()
+    try:
+        age = int(input('What is your age (years)? ').lower())
+        if age < 17:
+            print('Error: The minimum age is 17 years old.')
+            continue
+        break
+    except ValueError:
+        print('Error: Please enter a number.')
 
 while True:
     print()
@@ -110,46 +185,59 @@ while True:
 
 while True:
     print()
-    print('What percentage change per week?\n'
+    print('What change per week (units: %, lbs, kg)?\n'
           'Example:\n'
-          '-1.0 (Lose 1% per week)\n'
-          '0.25 (Gain 0.25% per week)')
+          '-1.0% (Lose 1% per week)\n'
+          '0.25 lbs (Gain 0.25 lbs per week)')
     try:
-        percentage_change = float(input('> '))
+        change, change_units = parse_input(input('> '), available_units=['%', 'lbs', 'kg'])
     except ValueError:
-        print('Error: Please enter a number.')
+        print()
+        print('Error: Please enter a number and units.')
         continue
     break
 
 
-def calculate_macros(tdee, extra, percentage_change, weight, body_fat_percentage, height, sex):
+def calculate_macros(tdee, extra, change, change_units, weight, weight_units, body_fat_percentage, height, height_units, sex):
     lean_body_mass = None
+
+    # Convert to KG
+    if weight_units == 'lbs':
+        weight = lbs_to_kg(weight)
+
+    if height_units == 'cm':
+        height = cm_to_inches(height)
 
     if body_fat_percentage:
         lean_body_mass = weight * (1 - body_fat_percentage / 100)
     else:
         height_difference = height - INITIAL_HEIGHT
-        if sex == 1:
+        if sex == 'm':
             ideal_weight = MALE_INITIAL_WEIGHT + (height_difference * 5)
-            lean_body_mass = ideal_weight * (1 - IDEAL_BFP_MALE / 100)
-        elif sex == 0:
+            lean_body_mass = lbs_to_kg(ideal_weight * (1 - IDEAL_BFP_MALE / 100))
+        elif sex == 'f':
             ideal_weight = FEMALE_INITIAL_WEIGHT + (height_difference * 5)
-            lean_body_mass = ideal_weight * (1 - IDEAL_BFP_FEMALE / 100)
+            lean_body_mass = lbs_to_kg(ideal_weight * (1 - IDEAL_BFP_FEMALE / 100))
 
-    protein_grams = int(lean_body_mass * GRAM_PROTEIN_LEAN_BODY_MASS)  # Set Minimum Protein
-    fat_grams = int(weight * MINIMUM_FAT_BODY_WEIGHT)  # Set Minimum Fat
+    protein_grams = int(lean_body_mass * GRAM_PROTEIN_LEAN_BODY_MASS_KG)  # Set Minimum Protein
+    fat_grams, omega3_grams, linoleic_acid_grams = calculate_fatty_acids(sex, age, lean_body_mass)
 
     protein_calories = protein_grams * CALORIES_PER_G_PROTEIN
     fat_calories = fat_grams * CALORIES_PER_G_FAT
 
-    lbs_change = weight * (percentage_change / 100)
-    if lbs_change < MAX_LBS_LOSS_PER_WEEK:  # Max lbs lost safety cap
-        lbs_change = MAX_LBS_LOSS_PER_WEEK
+    weight_change = None
+    if change_units == '%':
+        weight_change = weight * (change / 100)
+    elif change_units in ['lbs', 'kg']:
+        weight_change = change
 
-    elif lbs_change > MAX_LBS_GAIN_PER_WEEK:  # Max lbs gain safety cap
-        lbs_change = MAX_LBS_GAIN_PER_WEEK
+    if weight_change < MAX_KG_LOSS_PER_WEEK:  # Max lost safety cap
+        weight_change = MAX_KG_LOSS_PER_WEEK
 
-    daily_calorie_change = lbs_change * CALORIES_PER_LB_BODY_FAT / 7
+    elif weight_change > MAX_KG_GAIN_PER_WEEK:  # Max gain safety cap
+        weight_change = MAX_KG_GAIN_PER_WEEK
+
+    daily_calorie_change = weight_change * CALORIES_PER_KG_BODY_FAT / 7
     daily_calories = tdee + daily_calorie_change
     extra_calories = daily_calories - protein_calories - fat_calories
 
@@ -173,16 +261,28 @@ def calculate_macros(tdee, extra, percentage_change, weight, body_fat_percentage
 
     protein_calories = int(protein_grams * CALORIES_PER_G_PROTEIN)
     fat_calories = int(fat_grams * CALORIES_PER_G_FAT)
+    omega3_calories = int(omega3_grams * CALORIES_PER_G_FAT)
+    linoleic_acid_calories = int(linoleic_acid_grams * CALORIES_PER_G_FAT)
     carb_calories = int(carb_grams * CALORIES_PER_G_CARB)
 
-    return {
+    macros = {
         'protein': {
             'grams': protein_grams,
             'calories': protein_calories,
         },
         'fat': {
-            'grams': fat_grams,
-            'calories': fat_calories,
+            'total': {
+                'grams': fat_grams,
+                'calories': fat_calories,
+            },
+            'omega3': {
+                'grams': omega3_grams,
+                'calories': omega3_calories,
+            },
+            'linoleic_acid': {
+                'grams': linoleic_acid_grams,
+                'calories': linoleic_acid_calories,
+            }
         },
         'carb': {
             'grams': carb_grams,
@@ -190,13 +290,23 @@ def calculate_macros(tdee, extra, percentage_change, weight, body_fat_percentage
         }
     }
 
+    # Convert KGS to LBS
+    if weight_units == 'lbs':
+        lean_body_mass = kg_to_lbs(lean_body_mass)
+    lbm = {'weight': lean_body_mass, 'units': weight_units}
 
-macros = calculate_macros(tdee, extra, percentage_change, weight, body_fat_percentage, height, sex)
+    return macros, lbm
+
+
+macros, lbm = calculate_macros(tdee, extra, change, change_units, weight, weight_units, body_fat_percentage, height, height_units, sex)
 print()
 print(f"Protein: {macros['protein']['grams']}g ({macros['protein']['calories']} calories)")
-print(f"Fat: {macros['fat']['grams']}g ({macros['fat']['calories']} calories)")
+print(f"Fat: {macros['fat']['total']['grams']:.0f}g ({macros['fat']['total']['calories']} calories)")
+print(f"    Omega 3: {macros['fat']['omega3']['grams']:.2f}g ({macros['fat']['omega3']['calories']} calories)")
+print(f"    Linoleic Acid: {macros['fat']['linoleic_acid']['grams']}g ({macros['fat']['linoleic_acid']['calories']} calories)")
 print(f"Carbs: {macros['carb']['grams']}g ({macros['carb']['calories']} calories)")
 
-total_calories = macros['protein']['calories'] + macros['fat']['calories'] + macros['carb']['calories']
+total_calories = macros['protein']['calories'] + macros['fat']['total']['calories'] + macros['carb']['calories']
 print(f"Total Calories: {total_calories:,} kcal")
 print(f"Percentage Protein: {macros['protein']['calories'] / total_calories * 100:.2f}%")
+print(f"Learn Body Mass: {lbm['weight']:.0f} {lbm['units']}")
